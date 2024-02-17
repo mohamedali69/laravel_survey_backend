d4358016021c69e430cbbea5c587bc7f1559c36c
mohamedali69 <dalichakhari6801@gmail.com>
2024-02-17 20:04:08 +0100

    working on survey model

---
+++ app/Http/Controllers/SurveyController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Survey;
+use App\Http\Requests\StoreSurveyRequest;
+use App\Http\Requests\UpdateSurveyRequest;
+use App\Http\Resources\SurveyResource;
+use Symfony\Component\HttpFoundation\Request;
+
+class SurveyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        return SurveyResource::collection(
+            Survey::where('user_id', $user->id)
+                ->orderBy('created_at', 'desc')
+                ->paginate(2)
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSurveyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSurveyRequest $request, Survey $survey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Survey $survey)
+    {
+        //
+    }
+}
